6caeee12d3b123ca0b72fee82f6c9c98dc6abb2e
Jose María de la Cruz <jmariadelacruzsanchez@gmail.com>
2023-11-03 17:58:26 +0100

    update deposit and swap functions

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -2,5 +2,23 @@
 pragma solidity 0.8.19;
 
 contract Treasury {
-    
+
+    mapping(address => uint256) usersWETHBalance;
+    event ETHDeposited(address userAddress, uint256 amount);
+
+    // @notice: Function use for depositing ETH to the treasury
+    function depositWETH() public payable {
+        usersWETHBalance[msg.sender] += msg.value;
+        emit ETHDeposited(msg.sender, msg.value);
+    }
+
+    function swapWETHforUSDT(uint256 amount) public {
+        require(usersWETHBalance[msg.sender] >= amount, "Not enought WETH amount");
+        usersWETHBalance[msg.sender] -= amount;
+
+        
+    }
+
+
+  
 }
