f42bdb34a8764e991981c9d62af7a42e080bd00b
Jose María de la Cruz <jmariadelacruzsanchez@gmail.com>
2023-11-03 18:09:24 +0100

    add sushi and camelot routers

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -1,10 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
+import "./interfaces/IUniswapRouterV2.sol";
+import "./interfaces/IUniswapV2Router01.sol";
+
 contract Treasury {
 
-    mapping(address => uint256) usersWETHBalance;
+    IUniswapRouterV2 public uniswapV2Router;
+    IUniswapV2Router01 public camelotV2Router;
+
+    address public USDT;
+    mapping(address => uint256) public usersWETHBalance;
     event ETHDeposited(address userAddress, uint256 amount);
+
+    constructor(address USDTAddress, address uniswapV2RouterAddress, address camelotV2RouterAddress) {
+        uniswapV2Router = IUniswapRouterV2(uniswapV2RouterAddress);
+        camelotV2Router = IUniswapV2Router01(camelotV2RouterAddress);
+        USDT = USDTAddress;
+    }
 
     // @notice: Function use for depositing ETH to the treasury
     function depositWETH() public payable {
@@ -16,7 +29,7 @@ contract Treasury {
         require(usersWETHBalance[msg.sender] >= amount, "Not enought WETH amount");
         usersWETHBalance[msg.sender] -= amount;
 
-        
+
     }
 
 
