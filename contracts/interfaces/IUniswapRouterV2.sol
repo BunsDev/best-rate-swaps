3e666e19b32cce0b3e91dc7056d83fb72b0c1ea9
Jose María de la Cruz <jmariadelacruzsanchez@gmail.com>
2023-11-03 18:00:47 +0100

    add uniswap interface

---
+++ contracts/interfaces/IUniswapRouterV2.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.19;
+
+interface IUniswapRouterV2 {
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+}
