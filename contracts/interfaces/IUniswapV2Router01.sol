6134183c2def21846d5877fb6ac03db0676b5d5a
Jose María de la Cruz <jmariadelacruzsanchez@gmail.com>
2023-11-03 18:08:40 +0100

    fix format

--- contracts/interfaces/IUniswapV2Router01.sol
+++ contracts/interfaces/IUniswapV2Router01.sol
@@ -3,14 +3,12 @@
 pragma solidity 0.8.19;
 
 interface IUniswapV2Router01 {
-    
-function swapExactTokensForTokensSupportingFeeOnTransferTokens(
-    uint amountIn,
-    uint amountOutMin,
-    address[] calldata path,
-    address to,
-    address referrer,
-    uint deadline
-  ) external;
-
+    function swapExactTokensForTokensSupportingFeeOnTransferTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        address referrer,
+        uint deadline
+    ) external;
 }
